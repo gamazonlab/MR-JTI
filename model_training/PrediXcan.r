c5c8b3611f368a3b1da11dab741b7b071e9ecf1f
Dan Zhou <33189937+zdangm@users.noreply.github.com>
2019-11-16 12:39:41 -0600

    Update PrediXcan.r

--- model_training/PrediXcan.r
+++ model_training/PrediXcan.r
@@ -4,13 +4,13 @@
 
 args<-as.numeric(commandArgs(TRUE))
 library('glmnet')
-main_dir<-'/data/coxvgi/zhoud2/projects/cross_tissue/'
-v8_dir<-'/data/coxvgi/zhoud2/projects/v8/'
+folder='st'
 
+#set up sub jobs
 run_id<-1
 run_list<-list()
-for (i in 1:49){ #tissue id
-  for (j in 1:50){ #subjobs for each tissue
+for (i in 1:49){  #tissues
+  for (j in 1:60){  #subjobs per tissue
     run_list[[run_id]]<-c(i,j)
     run_id=run_id+1
   }
@@ -19,23 +19,20 @@ for (i in 1:49){ #tissue id
 run_i<-run_list[[args[1]]]
 print(run_i)
 
-folder='st' #predixcan
-
-#tissue name
-tissue_list<-dir(paste0(main_dir,'exp_residual/'))
-tissue_list<-as.character(sapply(tissue_list,function(x) strsplit(x,"[.]")[[1]][1]))
+#tissue
+tissue_list<-dir('/data/coxvgi/zhoud2/data/gtex/exp/v8/weighted/')
 tissue<-tissue_list[run_i[1]]
 
-#mkdir for output
-com<-paste0('mkdir ',v8_dir,'weights_cv/',folder,'/ ; mkdir ',v8_dir,'weights_cv/',folder,'/',tissue)
+#mkdir
+com<-paste0('mkdir /data/coxvgi/zhoud2/projects/gtex/weights/',folder,'/ ; mkdir /data/coxvgi/zhoud2/projects/gtex/weights/',folder,'/',tissue)
 system(command = com, wait = T)
 
 #get gene list
-exp_list<-sub('....$','',dir(paste0(main_dir,'exp_v8/',tissue)))  #expression gene list
-geno_list<-dir('/data/coxvgi/zhoud2/data/gtex/geno/v8/gene/p_1m/')  #genotype gene list
+exp_list<-sub('....$','',dir(paste0('/data/coxvgi/zhoud2/data/gtex/exp/v8/weighted/',tissue)))
+geno_list<-sub('....$','',dir('/data/coxvgi/zhoud2/data/gtex/geno/v8/gene/dosage_1m/'))
 gene_list<-intersect(exp_list,geno_list)
 
-#subjob start and end id
+#start and end id
 i_start=(run_i[2]-1)*500+1
 i_end=run_i[2]*500
 if (i_end>length(gene_list)){
@@ -50,33 +47,33 @@ if (i_start<length(gene_list)){
   for (i in i_start:i_end){
     print(i)
     #input geno and snp info
-    geno<-read.table(paste0('/data/coxvgi/zhoud2/data/gtex/geno/v8/gene/p_1m/',gene_list[i]),header=T,stringsAsFactors =F)
+    geno<-readRDS(paste0('/data/coxvgi/zhoud2/data/gtex/geno/v8/gene/dosage_1m/',gene_list[i],'.rds'))
     geno[,1]<-paste0('GTEX.',geno[,1])
-    snp_info<-read.table(paste0('/data/coxvgi/zhoud2/data/gtex/geno/v8/gene/p_1m_info/',gene_list[i]),header=T,stringsAsFactors =F)
+    snp_info<-readRDS(paste0('/data/coxvgi/zhoud2/data/gtex/geno/v8/gene/info_1m/',gene_list[i],'.rds'))
     
     #input expression levels
-    exp<-readRDS(paste0(main_dir,'exp_v8/',tissue,'/',gene_list[i],'.rds'))
-    exp<-exp[exp$tissue==tissue,]
+    exp<-readRDS(paste0('/data/coxvgi/zhoud2/data/gtex/exp/v8/weighted/',tissue,'/',gene_list[i],'.rds'))
+    exp<-exp[exp$tissue==tissue,] #only keeps expression in target tissue
     
     #merge
     d<-merge(exp,geno,by='sampleid')
     
     #-------------------
-    if(ncol(d)>5){  #skip genes with only one SNP.
-      y<-as.matrix(d[,2]); x<-as.matrix(d[,c(5:ncol(d))]) #assign x and y
-      set.seed(as.numeric(sub('^....','',gene_list[i]))) #use ensembl id as seed
-      #elastic net model training using 5-fold cv
-      fit<-cv.glmnet(x=x,y=y, nfolds = 5,keep = T,alpha=0.5,nlambda=50,pmax=200) 
+    if(ncol(d)>5){
+      y<-as.matrix(d[,2]); x<-as.matrix(d[,c(5:ncol(d))])
+      set.seed(as.numeric(sub('^....','',gene_list[i])))
+      #elastic net
+      fit<-cv.glmnet(x=x,y=y, nfolds = 5,keep = T,alpha=0.5,nlambda=50,pmax=200)  
       fit.df <- data.frame(fit$cvm,fit$lambda,1:length(fit$cvm))
-      best.lam <- fit.df[which.min(fit.df[,1]),] #find lambda with min cv error
+      best.lam <- fit.df[which.min(fit.df[,1]),]
       nrow.best = best.lam[,3] #position of best lambda in cv.glmnet output
       ret <- as.numeric(fit$glmnet.fit$beta[,nrow.best]) #beta
-      lambda<-as.numeric(fit$lambda.min) #lambda
+      lambda<-as.numeric(fit$lambda.min)
     }else{next}
     
     #correlation of predicted and observed expression
-    pred_exp = fit$fit.preval[,which(fit$lambda == fit$lambda.min)] #predicted expression
-    cor_test_t<-cor.test(y,pred_exp)  #correlation test
+    pred_exp = fit$fit.preval[,which(fit$lambda == fit$lambda.min)]
+    cor_test_t<-cor.test(y,pred_exp)
     r<-as.numeric(cor_test_t$estimate)
     p<-as.numeric(cor_test_t$p.value)
     
@@ -91,8 +88,8 @@ if (i_start<length(gene_list)){
     output<-snp_info
     
     #writeRDS
-    if (nrow(output)>0 & p<0.05 & r>0.1){ #only output iGene defined as p<0.05 $ r>0.10
-      out_path<-paste0(v8_dir,'weights_cv/',folder,'/',tissue,'/',gene_list[i],'.rds')
+    if (nrow(output)>0 & p<0.05 & r>0.1){
+      out_path<-paste0('/data/coxvgi/zhoud2/projects/gtex/weights/',folder,'/',tissue,'/',gene_list[i],'.rds')
       saveRDS(output,file=out_path)
     }
   }
