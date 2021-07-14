024ad1009119c3bfc7e3a7291b240d9ba0cfdddc
Dan Zhou <33189937+zdangm@users.noreply.github.com>
2021-07-14 18:25:06 -0500

    Update run.sh

--- model_training/predixcan/src/run.sh
+++ model_training/predixcan/src/run.sh
@@ -6,7 +6,7 @@
 
 #step 1. model training 
 #ml GCC OpenMPI R
-#Rscript ${main_dir}/src/predixcan.r \
+#Rscript ${main_dir}/src/predixcan_r.r \
 #        --model_training \
 #        --main_dir ${main_dir} \
 #        --plink_file_name ${main_dir}/chr22/chr22 \
@@ -20,7 +20,7 @@
 #ml GCC OpenMPI R
 
 #model_name=chr22 #the predix of output files
-#Rscript ${main_dir}/src/predixcan.r \
+#Rscript ${main_dir}/src/predixcan_r.r \
 #         --generate_db_and_cov \
 #         --main_dir ${main_dir} \
 #         --plink_file_name ${main_dir}/chr22/chr22 \
@@ -29,9 +29,25 @@
 #         --output_file_name ${model_name}
 
 
-#step 3. apply to gwas data
-#ml GCC/5.4.0-2.26  OpenMPI/1.10.3 pandas/0.18.1-Python-2.7.12 numpy/1.11.1-Python-2.7.12 scipy/0.17.0-Python-2.7.12
+#step 3. apply to gwas data (association test)
 
+#Option 1
+# Rscript ${src_dir}/predixcan_r.r \
+#         --asso_test \
+#         --db_path ${dir}/JTI_Liver.db \
+#         --cov_path ${dir}/JTI_Liver.txt.gz \
+#         --gwas_path ${dir}/LDLq.txt.gz \
+#         --gwas_variant_col rsid \
+#         --gwas_beta_col beta \
+#         --gwas_se_col se \
+#         --gwas_eff_allele_col eff_allele \
+#         --gwas_ref_allele_col ref_allele \
+#         --asso_out_path ${dir}/result_ldl_liver.txt \
+#         --parallel
+
+
+#Option 2
+#ml GCC/5.4.0-2.26  OpenMPI/1.10.3 pandas/0.18.1-Python-2.7.12 numpy/1.11.1-Python-2.7.12 scipy/0.17.0-Python-2.7.12
 #model_name=chr22
 
 #python2.7 ${your_dir_for_metaxcan_or_Spredixcan}/MetaXcan.py \
